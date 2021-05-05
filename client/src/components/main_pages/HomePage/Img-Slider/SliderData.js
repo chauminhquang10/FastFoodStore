280e72aa421ae49521a5b2a7e6968d6e646b7f81
Hao <18520716@gm.uit.edu.vn>
2021-05-05 23:24:13 +0700

    merge search bar, slider

--- client/src/components/main_pages/HomePage/Img-Slider/SliderData.js
+++ client/src/components/main_pages/HomePage/Img-Slider/SliderData.js
@@ -1,8 +1,8 @@
-import food1 from "../../../../images/doan1.jpg";
-import food2 from "../../../../images/doan2.jpg";
-import food3 from "../../../../images/doan3.jpg";
-import food4 from "../../../../images/doan4.jpg";
-import food5 from "../../../../images/doan5.jpg";
+import food1 from "../../../../images/new1.jpg";
+import food2 from "../../../../images/new2.jpg";
+import food3 from "../../../../images/new3.jpg";
+import food4 from "../../../../images/new4.jpg";
+import food5 from "../../../../images/new5.jpg";
 
 export const SliderData = [
   {
